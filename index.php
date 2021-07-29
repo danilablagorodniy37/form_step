39ea08e283c259223984d4bf1ccdc4c23a211c2c
unknown <zabavno-37@mail.ru>
2021-07-29 20:20:12 +0300

    array_form

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>form</title>
+</head>
+
+<body>
+
+    <div id="wrapper">
+       
+        <form id="signin" method="get" action="form_header.php" autocomplete="off">
+           
+            <input type="text" id="user" name="user" placeholder="username" />
+            
+            <input type="password" id="pass" name="pass" placeholder="password" />
+            
+            <button type="submit">&#xf0da;</button>
+            
+            <p>забыли пароль? <a href="#">клик</a></p>
+            
+        </form> 
+    </div>
+</body>
+
+</html>
+
